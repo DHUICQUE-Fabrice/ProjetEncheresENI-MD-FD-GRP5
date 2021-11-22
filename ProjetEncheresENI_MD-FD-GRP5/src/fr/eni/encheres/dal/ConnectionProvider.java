8521387460658deda4236609b40826c5dd285e1a
DHUICQUE-Fabrice <fabrice.dhuicque2021@campus-eni.fr>
2021-11-22 10:42:41 +0100

    Création du context.xml Création de la class ConnectionProvider

---
+++ ProjetEncheresENI_MD-FD-GRP5/src/fr/eni/encheres/dal/ConnectionProvider.java
@@ -0,0 +1,28 @@
+package fr.eni.encheres.dal;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ConnectionProvider {
+	private static DataSource dataSource;
+
+	static {
+		Context context;
+		try {
+			context = new InitialContext();
+			ConnectionProvider.dataSource = (DataSource) context.lookup("jdbc/pool_cnx_encheres");
+		} catch (NamingException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Impossible d'accéder à la bdd !");
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return ConnectionProvider.dataSource.getConnection();
+	}
+}
