19b9931f9a770c205792c418a90e55b0ae379dbc
DHUICQUE-Fabrice <fabrice.dhuicque2021@campus-eni.fr>
2021-11-26 08:56:14 +0100

    Fichier TODO.java renseigné (putain y a du taf ^^)

--- ProjetEncheresENI_MD-FD-GRP5/src/TodoClass.java
+++ ProjetEncheresENI_MD-FD-GRP5/src/TodoClass.java
@@ -1,6 +1,80 @@
 
 public class TodoClass {
-	// TODO CSS des jsp
-	// TODO importer liste encheres en cours dans page accueil
-	// TODO
+	// TODO CSS des jsp (in progress)
+	// TODO Vendre un article (in progress)
+	// TODO importer liste encheres en cours dans page accueil mode déconnecté
+	// TODO importer liste encheres en cours dans page accueil mode connecté
+	// TODO Page de gestion du profil (consultation, modification, suppression)
+	// TODO Visualisation du profil des autres utilisateurs
+	// TODO Faire une enchère
+	// TODO Lister ses ventes
+	// TODO Annuler une vente
+	// TODO Mot de passe oublié
+	// TODO Se souvenir de moi
+	// TODO Affichage du compte de points
+	// TODO Actions administrateur (Désactiver un compte utilisateur, supprimer des comptes, gérer les catégories)
+	// TODO Responsive design
+	
+	/*
+	 * 
+	 * Liste des pages devant exister :
+	 * 
+	 *  - Page d'accueil (créée, non complète):
+	 *  	******DECONNECTE******								******CONNECTE******
+	 * 		- S'inscrire / Se connecter (ok)					- Se déconnecter (ok)
+	 * 		- Affichage des enchères en cours					- Possibilité d'intéragir avec les enchères en cours
+	 * 		- Filtrer par catégorie								- Vendre un article (ok)
+	 * 		- Filtrer par mot clé (recherche)					- Mon profil
+	 * 		- Gestion des erreurs								- Filtrer achats/ventes (radio) avec sous filtres (en cours, terminé...) en checkBox
+	 * 		
+	 * 
+	 * 	- Page de connexion (créée, non complète)
+	 * 		- Rentrer son login et son mot de passe (ok)
+	 * 		- "Se souvenir de moi"
+	 * 		- Mot de passe oublié
+	 * 
+	 * 	- Page d'inscription (créée, complète)
+	 * 		- Rentrer ses informations (ok)
+	 * 		- Gestion des erreurs (ok, reste pseudo alphanumérique à vérifier)
+	 * 
+	 * 	- Page article (créée, non complète)
+	 * 		- Rentrer les informations de l'article (ok)
+	 * 		- Upload image
+	 * 
+	 * 	- Page Mon profil en mode consultation
+	 * 		- Affichage des infos du profil en mode consultation
+	 * 		- Bouton modifier pour passer en mode modifictaions
+	 * 
+	 * 	- Page Mon profil en mode édition (non créée)
+	 * 		- Affichage des champs en mode éditable
+	 * 		- Boutons enregistrer, supprimer compte et retour à l'accueil
+	 * 
+	 * 	- Page détail articles
+	 * 		- Affichage image article
+	 * 		- Affichage détails
+	 * 		- Pseudo du vendeur
+	 * 		- Possibilité d'enchérir (input type number avec valeur par défaut = meilleure offre + 10?)
+	 * 		- Bouton retour
+	 * 
+	 * 	- Page fin de l'enchère
+	 * 		- Dispo quand date_fin_encheres dépassée
+	 * 		- "Vous avez gagné" ou "Jeff Bezos a gagné" selon les cas
+	 * 		- Affichage mise à prix, prix final, vendeur, tel du vendeur, adresse du vendeur
+	 * 		- Confirmer retrait effectué
+	 * 
+	 * 	- Page articles en vente
+	 * 		- Possibilité annuler vente
+	 * 		- Affichage meilleure offre et infos de l'article
+	 * 
+	 * 
+	 * 	- Page article vendu
+	 * 		- Dispo quand date_fin_encheres dépassée
+	 * 		- Nom de l'acheteur
+	 * 		- Affichage mise à prix, prix final, infos vente
+	 * 		- Bouton "contacter acheteur"
+	 * 		- Bouton confirmation retrait
+	 * 
+	 * 
+	 * 
+	 */
 }
