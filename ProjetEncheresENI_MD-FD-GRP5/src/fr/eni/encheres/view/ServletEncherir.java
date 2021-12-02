2b15434c7ada118d2969e9d82d5207a1a42f9695
maximeDes <maxime.descatoire2021@campus-eni.fr>
2021-12-02 17:02:59 +0100

    push servlet enchere supprimer sans syso avec + commentaires

--- ProjetEncheresENI_MD-FD-GRP5/src/fr/eni/encheres/view/ServletEncherir.java
+++ ProjetEncheresENI_MD-FD-GRP5/src/fr/eni/encheres/view/ServletEncherir.java
@@ -59,7 +59,6 @@ public class ServletEncherir extends HttpServlet {
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/encherir.jsp");
 		requestDispatcher.forward(request, response);
 		
-		System.out.println("je suis dans le doGet");
 	}
 	
 	/**
@@ -76,26 +75,22 @@ public class ServletEncherir extends HttpServlet {
 		
 		
 		if (request.getParameter("action").equals("modifier")) {
-			
+			// modification d'un article par le vendeur. 
 			System.out.println("je suis dans le modifier");
 			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/modifierArticle.jsp");
 			requestDispatcher.forward(request, response);
 			
 		}else if (request.getParameter("action").equals("supprimer")) {
-			
+			// Suppresion d'un article par le vendeur.
 			ArticleManager art = new ArticleManager();
-			System.out.println(request.getParameter("article"));
-			
 			Article article = art.selectByIdArticle(Integer.parseInt(request.getParameter("article")));
-			System.out.println(article);
 			
 			deleteArticle(article, request, response);
-			System.out.println("je suis dans le supprimer");
 			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp");
 			requestDispatcher.forward(request, response);
 			
 		}else if (request.getParameter("action").equals("encherir")) {
-			
+			// Encherir sur un article d'un utilisateur par un acheteur.
 			Enchere enchere = new Enchere();
 			EnchereManager ench = new EnchereManager();
 			ArticleManager art = new ArticleManager();
@@ -120,14 +115,11 @@ public class ServletEncherir extends HttpServlet {
 				doGet(request, response);
 			}
 		}
-		System.out.println(request.getParameter("action") + "je suis juste dans le doPost");
 	}
 	
 	private void deleteArticle(Article article, HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		ArticleManager art = new ArticleManager();
-		
-		System.out.println(article.getIdArticle());
 		int id = article.getIdArticle();
 		try {
 			art.deleteArticle(id);
